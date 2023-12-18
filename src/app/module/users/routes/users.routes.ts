4c977dfd387c86c0324c65934db94902f2b836ba
Isaac Isvaldo Bunga <isaac.bunga@mamboo.co.ao>
2023-12-18 18:10:32 +0100

    doc: update documentation!

--- src/app/module/users/routes/users.routes.ts
+++ src/app/module/users/routes/users.routes.ts
@@ -2,7 +2,52 @@ import { Router } from 'express';
 
 import * as userController from '../controller/user.controller';
 const userRouter = Router();
-
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     description: Lista todos os Usuários
+ *     responses:
+ *       200:
+ *         description: Sucesso
+ */
 userRouter.get('/users', userController.list);
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Criar usuário
+ *     description: Cria um novo usuário com os dados fornecidos.
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         description: Dados do usuário a serem adicionados.
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             name:
+ *               type: string
+ *             email:
+ *               type: string
+ *               format: email
+ *             password:
+ *               type: string
+ *               format: password
+ *         example:
+ *           name: "Nome do usuário"
+ *           email: "usuario@example.com"
+ *           password: "senha123"
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso
+ *       400:
+ *         description: Requisição inválida - verifique os parâmetros enviados
+ */
+
 userRouter.post('/users', userController.create);
 export { userRouter };
